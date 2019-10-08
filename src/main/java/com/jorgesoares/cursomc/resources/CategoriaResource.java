b7aad0ca19a1f797850ed5474d0a4ffd9a24cbb4
Jorge Soares <jhorge.junior@gmail.com>
2019-10-08 15:13:10 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/jorgesoares/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jorgesoares/cursomc/resources/CategoriaResource.java
@@ -2,12 +2,17 @@ package com.jorgesoares.cursomc.resources;
 
 import com.jorgesoares.cursomc.domain.Categoria;
 import com.jorgesoares.cursomc.service.CategoriaService;
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -24,7 +29,14 @@ public class CategoriaResource {
     	Categoria obj = service.find(id);
     	return ResponseEntity.ok().body(obj);
 
-
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+    	obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 }
--- src/main/java/com/jorgesoares/cursomc/service/CategoriaService.java
+++ src/main/java/com/jorgesoares/cursomc/service/CategoriaService.java
@@ -19,4 +19,9 @@ public class CategoriaService {
         return obj.orElseThrow(() -> new ObjNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+
+    }
 }
