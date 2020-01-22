75b62f6b7920494af9c1a564c12a53ebc5b50be5
Jorge Soares <jhorge.junior@gmail.com>
2020-01-22 11:57:12 -0200

    Criado um profile de test

---
+++ src/main/java/com/jorgesoares/cursomc/config/TestConfig.java
@@ -0,0 +1,19 @@
+package com.jorgesoares.cursomc.config;
+
+import com.jorgesoares.cursomc.service.DBService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Bean
+    public boolean instantiateDatabase(DBService dbService) throws ParseException {
+            dbService.instantiateTestDatabase();
+        return true;
+    }
+}
