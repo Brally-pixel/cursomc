1b7cb352c499a8a8afb687b66c05e3b14e933290
Jorge Soares <jhorge.junior@gmail.com>
2020-02-26 22:53:16 -0200

    criado restricao para os endpoint

--- src/main/java/com/jorgesoares/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jorgesoares/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import com.jorgesoares.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,7 @@ public class CategoriaResource {
     	return ResponseEntity.ok().body(obj);
     }
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(
     		@Valid
@@ -45,6 +47,7 @@ public class CategoriaResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(
 	        @Valid
@@ -54,6 +57,7 @@ public class CategoriaResource {
     	service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}" , method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
--- src/main/java/com/jorgesoares/cursomc/resources/PedidoResource.java
+++ src/main/java/com/jorgesoares/cursomc/resources/PedidoResource.java
@@ -4,6 +4,7 @@ import com.jorgesoares.cursomc.domain.Pedido;
 import com.jorgesoares.cursomc.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
