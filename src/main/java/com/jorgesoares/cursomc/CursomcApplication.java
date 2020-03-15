b3b11ce550f4fd348ae6c4cbec60d413cf089ff6
Jorge Soares <jhorge.junior@gmail.com>
2020-03-15 11:55:44 -0200

    Configuracao do S3 da Amazon

--- src/main/java/com/jorgesoares/cursomc/CursomcApplication.java
+++ src/main/java/com/jorgesoares/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.jorgesoares.cursomc;
 
+import com.jorgesoares.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -14,5 +18,6 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        s3Service.uploadFile("D:\\Pastas\\Nova pasta\\img-eve-rework.jpg");
     }
 }
--- src/main/java/com/jorgesoares/cursomc/config/S3Config.java
+++ src/main/java/com/jorgesoares/cursomc/config/S3Config.java
@@ -1,4 +1,30 @@
 package com.jorgesoares.cursomc.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class S3Config {
+
+    @Value("${aws.secret_access_key}")
+    private String awsId;
+
+    @Value("${aws.access_key_id}")
+    private String awsKey;
+
+    @Value("${s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
+        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+    }
 }
